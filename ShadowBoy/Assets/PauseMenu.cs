cf1f44367283780cd3f21467bc458fd1eedd6502
MarcLaraM <mlaram@edu.tecnocampus.cat>
2025-05-14 17:58:11 +0200

    14/05/2025 Pruebas de movementplayer

--- ShadowBoy/Assets/PauseMenu.cs
+++ ShadowBoy/Assets/PauseMenu.cs
@@ -14,7 +14,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Escape))
+        if (Input.GetKeyUp(KeyCode.P))
         {
             if(isPaused)
             {
