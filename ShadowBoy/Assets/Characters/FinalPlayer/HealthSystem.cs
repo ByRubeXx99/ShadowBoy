eb596fb3282b1ec5da101930abe8718c9174d2d8
MarcLaraM <mlaram@edu.tecnocampus.cat>
2025-05-18 18:05:47 +0200

    Comenzando HealthSystem

---
+++ ShadowBoy/Assets/Characters/FinalPlayer/HealthSystem.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    [Header("Health System")]
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    [Header("Dead")]
+    public bool isDead = false;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (isDead) {  return; }
+
+        currentHealth -= damage;
+       
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);   // Hace que no baje de 0 ni suba de 100 de vida
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Die();
+        }
+    }
+
+
+    public void Die()
+    {
+        isDead = true;
+    }
+
+    public void ResetHealth()
+    {
+        isDead = false;
+        currentHealth = maxHealth;
+
+    }
+}
